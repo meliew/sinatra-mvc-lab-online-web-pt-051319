cc1067800fe1fe05567d36526c7a30ed5a2fe9f9
Melissa Williams <melissa@brandmeetscopy.com>
2019-08-17 20:18:02 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,23 +1,22 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-
+  
   get '/' do
     erb :user_input
   end
-
+  
   post '/piglatinize' do
-    @text = params[:user_phrase]
-
-    @user_phrase = PigLatinizer.new
-
+    
+    @text = Piglatinizer.new(params[:user_input])
+      
     erb :piglatinize
-end
-
-get '/piglatinize' do
-
-  erb :piglatinize
-end
-
-
+  end
+  
+  get '/piglatinize' do
+    
+    erb :piglatinize
+  end
+  
+  
 end
