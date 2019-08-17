5ff00c20cd06fdb73933fac74ec60d3195e59a92
Melissa Williams <melissa@brandmeetscopy.com>
2019-08-17 20:13:37 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -9,7 +9,7 @@ class App < Sinatra::Base
   post '/piglatinize' do
     @text = params[:user_phrase]
 
-    @pig_latin = PigLatinizer.new()
+    @pig_latin = PigLatinizer.new(params)
 
     erb :piglatinize
 end
