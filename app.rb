0a39eb112ba60c481876f60e0ab6a533644988af
Melissa Williams <melissa@brandmeetscopy.com>
2019-08-17 21:04:25 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ class App < Sinatra::Base
   end
 
   post '/piglatinize' do
-    @user_phrase = Piglatinizer.new(params[:user_phrase])
+    @user_phrase = params[:user_phrase]
 
     erb :piglatinize
 end
