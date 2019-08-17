0278f2f04de19014fa48f47b9fdc04e0b3b85212
Melissa Williams <melissa@brandmeetscopy.com>
2019-08-17 03:15:31 +0000

    Done.

---
+++ models/piglatinizer.rb
@@ -0,0 +1,20 @@
+class PigLatinizer
+
+  def piglatinize(word)
+    if word =~ /^[aeiou]/
+      "#{word}way"
+    else
+      #if there is a consonant
+      if word[0] !=~ /^[aeiou]/ && word[1] =~ /[aeiou]/
+        new_word = word.split(" ")
+        #how do I move the first letter to the end of the array?
+        #rejoin the word and add "ay" to the end
+        #Next - check if there is a second consonant after the first
+        #if there is, then add both consonants to the end of the word, and then add "way"
+        #then, if there are multiple words, I need to add them back together as a phrase
+        #put the phrase on the piglatinize.erb page
+    end
+  end
+
+
+end
