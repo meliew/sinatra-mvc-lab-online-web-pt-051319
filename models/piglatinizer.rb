9177e69b291483cabf837d0d75e2da9688ff7669
Melissa Williams <melissa@brandmeetscopy.com>
2019-08-17 20:07:33 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -18,7 +18,7 @@ end
       "#{word}way"
     elsif
       #if there is a consonant
-       word[0] !=~ /^[aeiou]/ && word[1] =~ /[aeiou]/
+       if word[0] !=~ /^[aeiou]/ && word[1] =~ /[aeiou]/
         new_word = word.split(" ")
         #how do I move the first letter to the end of the array?
         #rejoin the word and add "ay" to the end
